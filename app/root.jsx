c7e6f7a4c27a7807e1293c948d34f4cc9f333e6c
ahmad-56 <ahmad392.hassan@gmail.com>
2026-08-22 19:10:26 +0500

    favicon changed

--- app/root.jsx
+++ app/root.jsx
@@ -6,15 +6,22 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
-import "./app.css";
+
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+
+import "./app.css";
 
 export const links = () => [
   {
     rel: "icon",
-    type: "image/png",
-    href: "https://jt-innoventions25.vercel.app/og.png",
+    type: "image/x-icon",
+    href: "/favicon.ico?v=3",
+  },
+  {
+    rel: "shortcut icon",
+    type: "image/x-icon",
+    href: "/favicon.ico?v=3",
   },
 ];
 
@@ -23,23 +30,24 @@ export function Layout({ children }) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+
         <meta
           name="google-site-verification"
           content="Gea7yC6X5XPs53TWI4ss7y6pDV98nTTxUZ6YouJDkps"
-        />{" "}
-        {/* adding this meta html tag here for google search console */}
-        <link
-          rel="icon"
-          type="image/x-icon"
-          href="https://jt-innoventions25.vercel.app/og.png"
         />
+
         <Meta />
         <Links />
-        {/* Basic meta tags are sufficient for SEO */}
       </head>
+
       <body className="overflow-x-hidden">
         {children}
+
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -64,6 +72,7 @@ export function ErrorBoundary({ error }) {
 
   if (isRouteErrorResponse(error)) {
     message = error.status === 404 ? "404" : "Error";
+
     details =
       error.status === 404
         ? "The requested page could not be found."
@@ -74,11 +83,12 @@ export function ErrorBoundary({ error }) {
   }
 
   return (
-    <main className="pt-16 p-4 container mx-auto">
+    <main className="container mx-auto p-4 pt-16">
       <h1>{message}</h1>
       <p>{details}</p>
+
       {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
+        <pre className="w-full overflow-x-auto p-4">
           <code>{stack}</code>
         </pre>
       )}
